a397a4aee949f65fb1327177f0eb9a2e81c32deb
surajpahari <surajpahari152@gmail.com>
2023-06-24 15:03:33 +0545

    Employee creation

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -1,7 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Auth;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Illuminate\Validation\Rule;
@@ -11,17 +13,30 @@ class EmployeeController extends Controller
     public function index(){
         return Inertia::render("Employee");
     }
-    public function create(Request $req){
-        $this->validate($req,[
+    public function create(Request $request){
+        $this->validate($request,[
             'username' => ['required','string','max:20'],
             'name' => ['required','string','max:20'],
             'email' => ['required','email'],
-            'role_id' => ['required','integer',Rule::in([1,2])],
+            'role_id' => ['required','integer',Rule::in([0])],
             'payrate' => ['required','integer','min_digits:3','max_digits:6'],
             'password' => ['required','confirmed','min:6'],
             'phone_no' =>['required','integer','min_digits:10']
         ]);
-        return "validated";
+
+        $company = Auth::user()->company;
+
+        $employee = new User;
+        $employee->name = $request->input('name');
+        $employee ->username = $request->input('username');
+        $employee ->phone = $request->input('phone_no');
+        $employee ->email = $request->input('email');
+        $employee ->password = Hash::make($request->input('password'));
+        $employee ->role_id= $request->input('role_id');
+        $employee ->payrate = $request->input('payrate');
+
+        $company->user()->save($employee);
+        return $employee;
     }
     //
 }
