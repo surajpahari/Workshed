be89ccb071f16c51813583a67550b87adf42aef8
surajpahari <surajpahari152@gmail.com>
2023-06-25 12:14:06 +0545

    tailwind setup

--- resources/js/Pages/Components/Header.jsx
+++ resources/js/Pages/Components/Header.jsx
@@ -3,7 +3,15 @@ import { Link } from "@inertiajs/inertia-react";
 const Header = () => {
     return (
         <>
-            <h1>
+            <div className="bg-red-300">
+                <div className="flex">
+                    <div>
+                        WorkShed
+                    </div>
+
+                </div>
+            </div>
+            <h1 className="text-red-300">
                 Welcome to Workshed
             </h1>
             <Link href="/signin"
