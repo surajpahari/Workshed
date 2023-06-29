b5c54d59e25bfb4f08afe481a8f5b36488856fa6
surajpahari <surajpahari152@gmail.com>
2023-06-29 09:55:02 +0545

    fetching employeelist from api

--- resources/js/Pages/Employee/ListEmployee.jsx
+++ resources/js/Pages/Employee/ListEmployee.jsx
@@ -1,59 +1,39 @@
 
-const ListEmployee = ({ users }) => {
-    console.log(users);
+
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const ListEmployee = () => {
+    const [user, setUser] = useState(null);
+
+    async function getEmployeeList(pageNo = 1) {
+        try {
+            const url = `http://localhost:8000/api/employeeList/?page=${pageNo}`;
+            const response = await axios.get(url);
+            setUser(response.data);
+        } catch (error) {
+            // Handle the error here
+            console.error("Error fetching employee list:", error);
+        }
+    }
+
+    useEffect(() => {
+        getEmployeeList()
+            .catch(error => {
+                // Handle the error here
+                console.error("Error fetching employee list:", error);
+            });
+    }, []);
+
+    useEffect(() => {
+        console.log(user);
+    }, [user]);
+
     return (
         <>
-            <table className="table-auto">
-                <thead>
-                    <tr>
-                        <th>#Id</th>
-                        <th>Username</th>
-                        <th>Phone</th>
-                        <th>Email</th>
-                        <th>Role</th>
-                        <th>PayRate</th>
-                        <th>Joined Date</th>
-                        <th>Status</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
-                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
-                        <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-                        <td>Malcolm Lockyer</td>
-                        <td>1961</td>
-                    </tr>
-                    <tr>
-                        <td>Witchy Woman</td>
-                        <td>The Eagles</td>
-                        <td>1972</td>
-                        <td>Witchy Woman</td>
-                        <td>The Eagles</td>
-                        <td>1972</td>
-                        <td>Witchy Woman</td>
-                        <td>The Eagles</td>
-                        <td>1972</td>
-                    </tr>
-                    <tr>
-                        <td>Shining Star</td>
-                        <td>Earth, Wind, and Fire</td>
-                        <td>Shining Star</td>
-                        <td>Earth, Wind, and Fire</td>
-                        <td>1975</td>
-                        <td>Shining Star</td>
-                        <td>Earth, Wind, and Fire</td>
-                        <td>1975</td>
-                        <td>1975</td>
-                    </tr>
-                </tbody>
-            </table>
-
+            {user ? user.data.map((u) =>
+                <p>{u.name}</p>
+            ) : ''}
         </>
     );
 };
