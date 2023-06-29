a571e9243cc0ace88d2492634f6ea25ada0aeb26
surajpahari <surajpahari152@gmail.com>
2023-06-29 11:31:44 +0545

    implementation of react-paginate

--- resources/js/Pages/Employee/ListEmployee.jsx
+++ resources/js/Pages/Employee/ListEmployee.jsx
@@ -1,16 +1,17 @@
 
 
 import { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import axios from "axios";
 
 const ListEmployee = () => {
-    const [user, setUser] = useState(null);
+    const [response, setResponse] = useState(null);
 
     async function getEmployeeList(pageNo = 1) {
         try {
             const url = `http://localhost:8000/api/employeeList/?page=${pageNo}`;
             const response = await axios.get(url);
-            setUser(response.data);
+            setResponse(response.data);
         } catch (error) {
             // Handle the error here
             console.error("Error fetching employee list:", error);
@@ -26,17 +27,45 @@ const ListEmployee = () => {
     }, []);
 
     useEffect(() => {
-        console.log(user);
-    }, [user]);
+        console.log(response);
+    }, [response]);
+
+
 
     return (
         <>
-            {user ? user.data.map((u) =>
-                <p>{u.name}</p>
-            ) : ''}
+            <div>
+                {response ? response.data.map((u, index) =>
+                    <p key={index}>{u.name}</p>
+                ) : ''}
+            </div>
+            {
+                response ?
+                    <div className="paginate-container border-solid border-black border-2">
+                        <ReactPaginate
+                            nextLabel='next >'
+                            previousLabel='<previous'
+                            onPageChange={(pageNumber) => {
+                                console.log(pageNumber.selected)
+                                getEmployeeList(pageNumber.selected + 1)
+                            }}
+                            pageCount={response.to - 1}
+                            activeClassName='activePage'
+                            previousClassName='previousPage'
+                            className="flex flex-grow"
+                            pageRangeDisplayed={1}
+                            marginPagesDisplayed={1}
+                        />
+                    </div>
+                    : ''
+
+            }
+
         </>
     );
+
 };
+
 
 export default ListEmployee;
 
