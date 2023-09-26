9aed05220415166110c560285b49b1886749bc28
dixyant <adhikaridikshyant@gmail.com>
2023-09-26 18:07:50 +0545

    clear SelectSearch component after form submission

--- resources/js/Pages/Jobs/Tasks/AddTask.jsx
+++ resources/js/Pages/Jobs/Tasks/AddTask.jsx
@@ -1,9 +1,22 @@
+
 import { useForm } from "@inertiajs/inertia-react"
 import axios from "axios"
 import { useEffect, useState } from "react"
 import SearchSelect from "./SearchSelect"
 import { toast } from "react-hot-toast"
+
 const AddTask = () => {
+    const getCurrentDate = () => {
+        const now = new Date();
+        const year = now.getFullYear();
+        const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Month is zero-based
+        const day = now.getDate().toString().padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    };
+    const [clearEmployee, setClearEmployee] = useState(false);
+    const [clearType, setClearType] = useState(false);
+    const [clearLocation, setClearLocation] = useState(false);
+
     const { data, setData, reset, clearErrors, errors, post, processing } = useForm({
         type: null,
         location: null,
@@ -12,28 +25,35 @@ const AddTask = () => {
         endDate: '',
         startTime: '',
         endTime: '',
-    })
+    });
+
     const handleSubmit = (e) => {
-        e.preventDefault()
+        e.preventDefault();
         post('/job', {
             onSuccess: () => {
-                reset()
-                toast.success("New Job Created sucessfully")
+                reset();
+                toast.success("New Job Created successfully");
+                // Invoke the hardReset function in each SearchSelect component
+                setClearType(true)
+                setClearEmployee(true)
+                setClearLocation(true)
             },
             onError: () => (toast.error("Error creating new Job"))
-        })
-    }
+        });
+    };
 
     const dataSetter = (data, value) => {
-        setData(data, value)
-        clearErrors(data)
-    }
-    const dataRemover = (data) => {
-        reset(data)
-    }
+        setData(data, value);
+        clearErrors(data);
+    };
+
+    const dataRemover = (data = undefined) => {
+        reset(data);
+    };
+
     useEffect(() => {
-        console.log(data)
-    }, [data])
+        console.log(data);
+    }, [data]);
 
     return (
         <div>
@@ -41,34 +61,63 @@ const AddTask = () => {
                 New Job
             </div>
             <form onSubmit={handleSubmit}>
-                <SearchSelect title="Employee" optionLink="http://localhost:8000/employee-option"
-                    label="name" setter={dataSetter} fordata="employee" reseter={dataRemover} error={errors.employee} />
-                <SearchSelect title="Job-Type" optionLink="http://localhost:8000/type-option"
-                    label="type" fordata="type" setter={dataSetter} reseter={dataRemover} error={errors.type} />
-                <SearchSelect title="Location" optionLink="http://localhost:8000/location-option"
-                    label="name" fordata="location" setter={dataSetter} reseter={dataRemover} error={errors.location} />
+                <SearchSelect
+                    title="Employee"
+                    optionLink="http://localhost:8000/employee-option"
+                    label="name"
+                    fordata="employee"
+                    setter={dataSetter}
+                    reseter={dataRemover}
+                    error={errors.employee}
+                    clear={clearEmployee}
+                    setClear={setClearEmployee}
+                />
+                <SearchSelect
+                    title="Job-Type"
+                    optionLink="http://localhost:8000/type-option"
+                    label="type"
+                    fordata="type"
+                    setter={dataSetter}
+                    reseter={dataRemover}
+                    error={errors.type}
+                    clear={clearType}
+                    setClear={setClearType}
+                />
+                <SearchSelect
+                    title="Location"
+                    optionLink="http://localhost:8000/location-option"
+                    label="name"
+                    fordata="location"
+                    setter={dataSetter}
+                    reseter={dataRemover}
+                    error={errors.location}
+                    clear={clearLocation}
+                    setClear={setClearEmployee}
+                />
 
                 {/* for the start date and end date*/}
-                <div className="m-1">
-                    <label htmlFor="employee" className="font-bold text-lg">StartAt</label>
+                <div className="m-2">
+                    <label htmlFor="employee" className="font-bold ">StartAt</label>
                 </div>
                 <div className="flex m-1">
                     <div className="m-1">
                         <input
                             required
-                            className="outline-none border-none bg-sky-100 min-w-full text-xl"
+                            className="outline-none border-none bg-sky-50 min-w-full text-xl"
+                            value={data.startDate}
                             type="date" name="startDate" placeholder="startDate" onChange={(e) => {
-                                setData("startDate", e.target.value)
-                                clearErrors("startDate")
+                                setData("startDate", e.target.value);
+                                clearErrors("startDate");
                             }} />
                     </div>
                     <div>
                         <input
                             required
-                            className="outline-none border-none bg-sky-100 min-w-full text-xl"
+                            className="outline-none border-none bg-sky-50 min-w-full text-xl"
+                            value={data.startTime}
                             type="time" name="startTime" placeholder="startTime" onChange={(e) => {
-                                setData("startTime", e.target.value)
-                                clearErrors("startTime")
+                                setData("startTime", e.target.value);
+                                clearErrors("startTime");
                             }} />
                     </div>
                 </div>
@@ -77,29 +126,30 @@ const AddTask = () => {
                         errors.startDate ? <div>{errors.startDate}</div> : ''
                 }
 
-
-
-                <div className="m-1">
-                    <label htmlFor="employee" className="font-bold text-lg">EndAt</label>
+                <div className="m-2">
+                    <label htmlFor="employee" className="font-bold ">EndAt</label>
                 </div>
                 <div className="flex m-1">
-                    <div className="mr-1">
+                    <div className="m-1">
                         <input
+                            min={getCurrentDate}
                             required
-                            className="outline-none border-none bg-sky-100 min-w-full text-xl"
+                            className="outline-none border-none bg-sky-50 min-w-full text-xl"
+                            value={data.endDate}
                             type="date" name="endDate" placeholder="endDate" onChange={(e) => {
-                                setData("endDate", e.target.value)
-                                clearErrors("endDate")
+                                setData("endDate", e.target.value);
+                                clearErrors("endDate");
                             }} />
                     </div>
 
                     <div>
                         <input
                             required
-                            className="outline-none border-none bg-sky-100 min-w-full text-xl"
+                            className="outline-none border-none bg-sky-50 min-w-full text-xl"
+                            value={data.endTime}
                             type="time" name="endTime" placeholder="endTime" onChange={(e) => {
-                                setData("endTime", e.target.value)
-                                clearErrors("endTime")
+                                setData("endTime", e.target.value);
+                                clearErrors("endTime");
                             }} />
                     </div>
                 </div>
@@ -109,15 +159,16 @@ const AddTask = () => {
                 }
                 <div className="bg-gray-100  p-2 flex justify-end">
                     <div className="text-lg">
-                        <input type="submit" name="submit" value="submit"
+                        <input
+                            type="submit" name="submit" value="submit"
                             required
                             className="cursor-pointer text-lg p-1 py-2 bg-teal-500 border-none rounded text-white" />
                     </div>
                 </div>
+            </form>
+        </div>
+    );
+};
 
-            </form >
-        </div >
-    )
-}
+export default AddTask;
 
-export default AddTask
--- resources/js/Pages/Jobs/Tasks/SearchSelect.jsx
+++ resources/js/Pages/Jobs/Tasks/SearchSelect.jsx
@@ -1,93 +1,100 @@
-import { options } from "@fullcalendar/core/preact";
+
+
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faClose } from "@fortawesome/free-solid-svg-icons";
 
-const SearchSelect = ({ title, optionLink, label, fordata, setter, reseter, error }) => {
+const SearchSelect = ({ title, optionLink, label, fordata, setter, reseter, error, clear, setClear }) => {
     const [input, setInput] = useState('');
     const [mainOptions, setMainOptions] = useState(null);
-    const [optionSatus, changeOptionStatus] = useState(false);
+    const [optionStatus, changeOptionStatus] = useState(false);
     const [filteredOptions, setFilteredOptions] = useState(mainOptions);
     const [inputStatus, changeInputStatus] = useState(false);
     const [resetStatus, changeResetStatus] = useState(false);
     const [selected, setSelected] = useState(null);
     const [optionHover, setOptionHover] = useState(false);
+
     async function fetchOption() {
         try {
-            const url = optionLink
+            const url = optionLink;
             const response = await axios.get(url);
             setMainOptions(response.data);
         } catch (error) {
-            // Handle the error here
-            console.error("Error fetching employee list:", error);
+            console.error("Error fetching option:", error);
         }
     }
-    useEffect(() => {
-        fetchOption()
-        console.log("hello")
-    }, [])
-    useEffect(() => {
 
-    }, [mainOptions])
+    useEffect(() => {
+        fetchOption();
+    }, []);
 
     useEffect(() => {
         if (input.trim() === '') {
-            setFilteredOptions(mainOptions)
+            setFilteredOptions(mainOptions);
+        } else {
+            setFilteredOptions(filterForInput(input));
         }
-        else {
-            setFilteredOptions(filterForInput(input))
+    }, [input]);
+    useEffect(() => {
+        if (clear) {
+            hardReset()
+            setClear(false)
         }
-    }, [input])
+    }, [clear])
+
     const handleChange = (e) => {
-        setInput(e.target.value)
-        if (e.target.value == "") {
-            e.preventDefault()
+        setInput(e.target.value);
+        if (e.target.value === "") {
+            e.preventDefault();
         }
-        //change the <input type="" name="" value="">
-        //filter the option along with the input
-        //if pressed enter setSelected the first item of the input
-    }
+    };
+
     const handleFocus = () => {
         if (!inputStatus) {
-            changeOptionStatus(true)
+            changeOptionStatus(true);
+        } else {
+            changeOptionStatus(false);
         }
-        else {
-            changeOptionStatus(false)
-        }
-
-    }
+    };
 
     const handleBlur = (e) => {
         if (!optionHover) {
             changeOptionStatus(false);
         }
-    }
+    };
+
     const handleKeyDown = (e) => {
         if (e.key === 'Enter') {
-            e.preventDefault()
+            e.preventDefault();
             if (filteredOptions.length > 0) {
-                setInput(filteredOptions[0][label])
-                changeOptionStatus(false)
-                changeInputStatus(true)
-                changeResetStatus(true)
-                setSelected(filteredOptions[0].id)
+                setInput(filteredOptions[0][label]);
+                changeOptionStatus(false);
+                changeInputStatus(true);
+                changeResetStatus(true);
+                setSelected(filteredOptions[0].id);
             }
         }
-    }
+    };
+
     const handleOptionClick = (option) => {
-        setInput(option[label])
-        changeOptionStatus(false)
-        changeInputStatus(true)
-        changeResetStatus(true)
-        setSelected(option.id)
-    }
-    const handleReset = (e) => {
+        setInput(option[label]);
+        changeOptionStatus(false);
+        changeInputStatus(true);
+        changeResetStatus(true);
+        setSelected(option.id);
+    };
+
+    const hardReset = (e) => {
         setInput('');
-        changeResetStatus(false)
-        changeInputStatus(false)
-        reseter(fordata)
-    }
+        changeResetStatus(false);
+        changeInputStatus(false);
+        reseter(fordata);
+    };
+
+    const handleReset = (e) => {
+        hardReset();
+    };
 
     const filterForInput = (prefix) => {
         prefix = prefix.toLowerCase();
@@ -97,25 +104,22 @@ const SearchSelect = ({ title, optionLink, label, fordata, setter, reseter, erro
     };
 
     useEffect(() => {
-        setter(fordata, selected)
+        setter(fordata, selected);
+    }, [selected]);
 
-    }, [selected])
     return (
         <>
             <div className="m-1 flex-col">
                 <div className="m-1">
-                    <label htmlFor="employee" className="font-bold text-lg">{title}</label>
+                    <label htmlFor="employee" className="font-bold">{title}</label>
                 </div>
                 <div className="relative">
                     <div className="relative">
                         <div>
-                            <input type="text"
+                            <input
+                                type="text"
                                 required
-                                className={`outline-none border-none bg-sky-100 min-w-full text-xl
-p-2
-                                ${inputStatus ? 'bg-teal-400 text-white rounded' : 'bg-sky-100'}
-                                `
-                                }
+                                className={`outline-none border-none bg-sky-50 min-w-full text-xl p-2 ${inputStatus ? 'bg-teal-400 text-white rounded' : 'bg-sky-100'}`}
                                 placeholder={title}
                                 value={input}
                                 onChange={handleChange}
@@ -142,12 +146,12 @@ p-2
                         </div>
                         : ""
                     }
-                    {optionSatus ?
-                        <div className="absolute  w-full z-10"
+                    {optionStatus && (filteredOptions ? filteredOptions.length !== 0 : false) ?
+                        <div className="absolute w-full z-10"
                             onMouseOver={() => { setOptionHover(true) }}
                             onMouseOut={() => { setOptionHover(false) }}
                         >
-                            <div className="max-h-32 bg-white p-2  border-teal-500 overflow-y-auto">
+                            <div className="max-h-32 bg-white p-2 border-teal-500 overflow-y-auto">
                                 {
                                     filteredOptions ?
                                         filteredOptions.map((option, index) => (
@@ -174,9 +178,10 @@ p-2
                         : ''
                     }
                 </div>
-            </div >
+            </div>
         </>
-    )
-}
+    );
+};
 
-export default SearchSelect
+export default SearchSelect;
+
